25a59ac8447aa9a12691f6b65bf59a818720ebc7
Alex Muthmann <amuthmann@dev-eth0.de>
2022-07-05 21:15:34 +0200

    Basic qmk configs for RecyclingPad v1x

--- qmk/recyclingpad/v1x_triple/keymaps/default/keymap.c
+++ qmk/recyclingpad/v1x_triple/keymaps/default/keymap.c
@@ -17,6 +17,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include QMK_KEYBOARD_H
 
+/**
+ * NOTE: ROWS and COLs are flipped
+ */
+
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
@@ -30,9 +34,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     \---------|---------|---------|
     */
     [0] = LAYOUT(
-                    KC_P7,      KC_P8,    KC_P9,             
-                    KC_P4,      KC_P5,    KC_P6,             
-                    KC_P1,      KC_P2,    KC_P3              
+                    KC_P3,      KC_P6,    KC_P9,             
+                    KC_P2,      KC_P5,    KC_P8,             
+                    KC_P1,      KC_P4,    KC_P7              
     ),
 };
 // clang-format on
