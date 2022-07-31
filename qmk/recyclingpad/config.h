481ba2e42916df533e4bacfd3475265afb6f59e8
Alex Muthmann <deveth0@users.noreply.github.com>
2022-07-31 18:54:28 +0200

    Unique Product ID

--- qmk/recyclingpad/config.h
+++ qmk/recyclingpad/config.h
@@ -20,9 +20,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x0913
-#define MANUFACTURER    imchipwood
-#define PRODUCT         dumbpad
+#define PRODUCT_ID      0x1312
+#define MANUFACTURER    deveth0
+#define PRODUCT         recyclingPad
 
 /* Column/Row IO definitions */
 #define DIODE_DIRECTION COL2ROW
