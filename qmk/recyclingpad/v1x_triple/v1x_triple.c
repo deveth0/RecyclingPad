254974cfafcfa582ad8797c0377c3660da43251c
Alex Muthmann <deveth0@users.noreply.github.com>
2023-02-20 20:56:45 +0100

    Update v1x_triple.c

--- qmk/recyclingpad/v1x_triple/v1x_triple.c
+++ qmk/recyclingpad/v1x_triple/v1x_triple.c
@@ -1,5 +1,4 @@
 /* Copyright 2022 deveth0
-/* Copyright 2022 deveth0
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
